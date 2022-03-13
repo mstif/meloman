c4994b260b786c0e7791f02c779fd3e0eec9c2dc
mstisf <mstisf@gmail.com>
2022-03-13 23:08:14 +0300

    Rename variables

--- src/main/kotlin/ru/netology/PurchaseAmount.kt
+++ src/main/kotlin/ru/netology/PurchaseAmount.kt
@@ -2,27 +2,27 @@ package ru.netology
 
 import kotlin.math.roundToInt
 
-const val percentForRegularFee = 1
-const val percentForSecondFee = 5
-const val firstFee = 100
+const val percentForRegularDiscount = 1
+const val percentForSecondDiscount = 5
+const val firstDiscount = 100
 fun main() {
     val previousAmountCustomer = 15000
     val isRegularCustomer = true
-    val totalAmount = 150
+    val totalAmount = 1500
     println(
         "Общая сумма покупки: $totalAmount , со скидкой: " +
-                "${getAmountWithFee(totalAmount, previousAmountCustomer, isRegularCustomer)} рублей"
+                "${getAmountWithDiscount(totalAmount, previousAmountCustomer, isRegularCustomer)} рублей"
     )
 }
 
-fun getAmountWithFee(totalAmount: Int, previousAmount: Int, isRegularCustomer: Boolean): Int {
-    val regularFee = (if (isRegularCustomer) (100 - percentForRegularFee) else 100) / 100.0
+fun getAmountWithDiscount(totalAmount: Int, previousAmount: Int, isRegularCustomer: Boolean): Int {
+    val regularDiscount = (if (isRegularCustomer) (100 - percentForRegularDiscount) else 100) / 100.0
     val result: Double = (if (previousAmount <= 1000)
         totalAmount
-    else if (totalAmount <= firstFee) 0 else
+    else if (totalAmount <= firstDiscount) 0 else
         if (previousAmount < 10000)
-            (totalAmount - firstFee)
-        else (totalAmount - firstFee) * (100 - percentForSecondFee) / 100) * regularFee
+            (totalAmount - firstDiscount)
+        else (totalAmount - firstDiscount) * (100 - percentForSecondDiscount) / 100) * regularDiscount
     return result.roundToInt()
 
 }
